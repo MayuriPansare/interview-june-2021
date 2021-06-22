125f821a092642997f6d3b766cd927bc53128a85
MayuriPansare <85223449+MayuriPansare@users.noreply.github.com>
2021-06-22 12:36:27 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,10 +1,36 @@
-package exam;
+class RotateRight {    
+ public static void main(String[] args) {    
+       
+        int [] arr = new int [] {1, 2, 3, 4, 5};     
+          
+        int n = 3;    
+           
+        //Displays original array    
+        System.out.println("Original array: ");    
+        for (int i = 0; i < arr.length; i++) {     
+            System.out.print(arr[i] + " ");     
+        }      
+            
+           
+        for(int i = 0; i < n; i++){    
+            int j, last;    
+            //Stores the last element of array    
+            last = arr[arr.length-1];    
+            
+            for(j = arr.length-1; j > 0; j--){    
+                //Shift element of array by one    
+                arr[j] = arr[j-1];    
+            }    
 
-public class ShiftElementsInArray {
-
-  public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-
-  }
-
-}
+            arr[0] = last;    
+        }    
+        
+        System.out.println();    
+            
+           
+        System.out.println("Array after right rotation: ");    
+        for(int i = 0; i< arr.length; i++){    
+            System.out.print(arr[i] + " ");    
+        }    
+    }    
+}    
